8b5d85953d3b413ecaf3880f2c4dd5cc5fc8abc7
Sergio Segrera <sergioarturosegrera@gmail.com>
2023-01-19 13:57:45 -0500

    Fix google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,17 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next()],
+  integrations: [
+    tailwind(),
+    astroI18next(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
