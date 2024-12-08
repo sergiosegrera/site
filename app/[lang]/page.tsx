03e46d9a305bfe9999bc66585361d4263fe27737
Sergio Segrera <sergioarturosegrera@gmail.com>
2024-12-08 14:35:43 -0800

    update site

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,16 +1,71 @@
 import { getDictionary } from "@/get-dictionaries";
 import { Locale } from "@/i18n-config";
+import Image from "next/image";
+import {
+  LucideExternalLink,
+  LucideGithub,
+  LucideLinkedin,
+  LucideMail,
+  LucideMapPin,
+} from "lucide-react";
+import Link from "next/link";
+import Projects from "./_components/projects";
+import Bio from "./_components/bio";
+import Contact from "./_components/contact";
+import LanguageSwitcher from "./_components/language-switcher";
 
-export default async function Home({
-  params: { lang },
-}: {
-  params: { lang: Locale };
+export default async function Home(props: {
+  params: Promise<{ lang: Locale }>;
 }) {
+  const params = await props.params;
+
+  const { lang } = params;
+
   const dictionary = await getDictionary(lang);
 
   return (
-    <main className="md:w-[600px] mx-auto">
-      <h1>{dictionary.title}</h1>
+    <main className="md:w-[600px] mx-auto mt-16 p-4 flex flex-col gap-12">
+      {/* Header */}
+      <div id="header" className="grid grid-cols-[64px_1fr_auto] gap-4 w-full">
+        {/* <div id="logo" className="w-16 h-16 bg-slate-200 rounded-lg"></div> */}
+        <Image
+          src="/static/profile.webp"
+          alt="profile"
+          width={64}
+          height={64}
+          className="rounded-lg shadow-md"
+        />
+        <div id="info" className="flex flex-col h-full justify-between gap-1">
+          <h1 className="text-base font-medium">Sergio Segrera</h1>
+          <p className="text-xs text-slate-500">
+            {dictionary.title} @{" "}
+            <Link
+              href="https://clarum.ai"
+              className="inline-flex items-center gap-1 hover:underline"
+            >
+              {dictionary.company}
+              <LucideExternalLink size={12} />
+            </Link>
+          </p>
+          <p className="text-xs text-slate-500 flex flex-row items-center">
+            <LucideMapPin
+              className="inline-block mr-1 text-slate-400"
+              size={12}
+            />
+            {dictionary.location}
+          </p>
+        </div>
+        <div id="lang-switcher" className="flex justify-end">
+          <LanguageSwitcher currentLang={lang} />
+        </div>
+      </div>
+      {/* Bio */}
+      <Bio dictionary={dictionary} />
+      {/* Projects */}
+      <Projects dictionary={dictionary} />
+
+      {/* Contact */}
+      <Contact dictionary={dictionary} />
     </main>
   );
 }
